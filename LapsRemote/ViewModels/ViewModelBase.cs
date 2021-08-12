a23778440b80bb45d63a1706d3ff09f62fa801f4
Timothy Ganoza <jostimian@gmail.com>
2021-08-12 23:23:36 +0800

    First Commit

---
+++ LapsRemote/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LapsRemote.ViewModels
+{
+	public class ViewModelBase : ReactiveObject
+	{
+	}
+}
